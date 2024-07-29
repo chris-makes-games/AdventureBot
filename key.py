9c732a7b13338ea43aae6fe51427b9957d79ffaa
chris-makes-games <chris.m.cox@hotmail.com>
2024-07-29 17:53:39 +0000

    added combine field to key

--- key.py
+++ key.py
@@ -4,7 +4,7 @@ import database
 class Key:
   def __init__(self, id=None,
     displayname=None, subkeys=None, author=None, note=None, alt_note=None, description=None, 
-deconstruct=False, unique=False, repeating=False, stackable=False):
+combine = False, deconstruct=False, unique=False, repeating=False, stackable=False):
     if not id:
       self.id = database.generate_unique_id()
     else:
@@ -17,6 +17,7 @@ deconstruct=False, unique=False, repeating=False, stackable=False):
     self.alt_note = alt_note
 
     #boolean attributes
+    self.combine = combine
     self.deconstruct = deconstruct
     self.unique = unique
     self.repeating = repeating
