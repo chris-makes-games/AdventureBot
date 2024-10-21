05e0df1eb2460271f17bcdc25ae019435282d93c
chris-makes-games <chris.m.cox@hotmail.com>
2024-10-21 15:57:36 +0000

    added journal/inventory bools back to key class and create/edit methods. Can't remember why I deleted them. Also fixed combine field for class

--- key.py
+++ key.py
@@ -4,7 +4,7 @@ import database
 class Key:
   def __init__(self, id=None,
     displayname=None, subkeys=None, author=None, note=None, alt_note=None, description=None, 
-combine = False, deconstruct=False, unique=False, repeating=False, stackable=False):
+combine = False, inventory=False, journal=False deconstruct=False, unique=False, repeating=False, stackable=False):
     if not id:
       self.id = database.generate_unique_id()
     else:
@@ -22,6 +22,8 @@ combine = False, deconstruct=False, unique=False, repeating=False, stackable=Fal
     self.unique = unique
     self.repeating = repeating
     self.stackable = stackable
+    self.inventory = inventory
+    self.journal = journal
 
     #subkeys now a dict
     if subkeys is None:
