c2c898ca99e0c5aa1030d78914187d9d737c4b09
chris-makes-games <chris.m.cox@hotmail.com>
2024-10-28 15:44:25 -0400

    Update ping.py

--- cmds/ping.py
+++ cmds/ping.py
@@ -6,7 +6,7 @@ from discord.ext import commands
 async def ping(ctx):
   bot = ctx.bot
   print('Pong!! 🏓\n {0}'.format(round(bot.latency, 2)) + " seconds")
-  await ctx.reply('Pong!! 🏓\n {0}'.format(round(bot.latency, 2)) + " seconds")
+  await ctx.reply('Pong!! 🏓\n {0}'.format(round(bot.latency, 2)) + " seconds", ephemeral=True)
 
 async def setup(bot):
   bot.add_command(ping)
